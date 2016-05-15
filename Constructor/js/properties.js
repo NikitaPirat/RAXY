e6544e5b0ec8f4a4716f029731cb874663c1a4a5
Fedorov <I326727@global.corp.sap>
2016-05-15 17:35:38 +0400

    car added, getObjectsByClass added

--- Constructor/js/properties.js
+++ Constructor/js/properties.js
@@ -1,2 +1,3 @@
 var roadProperties = ['name', 'obj_class', 'obj_type', 'lines', 'marking'];
 var lineProperties = ['name', 'obj_class', 'obj_type', 'left', 'right'];
+var carProperties = ['name', 'obj_class', 'obj_type', 'number', 'right direction indicator', 'left direction indicator'];
