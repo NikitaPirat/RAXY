5c5d667e4fd68432fc65491f3e1d747c1eeaa71e
NikitaPirat <nikita_fed_92-92@mail.ru>
2016-05-17 23:34:12 +0400

    creation and download of json file added, icons added

--- Constructor/js/properties.js
+++ Constructor/js/properties.js
@@ -2,3 +2,13 @@ var sharedProperties = [ 'name', 'obj_class', 'obj_type']
 var roadProperties = [ 'lines', 'marking'];
 var lineProperties = ['left', 'right'];
 var carProperties = ['number', 'right direction indicator', 'left direction indicator'];
+
+function createJsonProperties(){
+    var result = [];
+    Array.prototype.push.apply(result, sharedProperties);
+    Array.prototype.push.apply(result, roadProperties);
+    Array.prototype.push.apply(result, lineProperties);
+    Array.prototype.push.apply(result, carProperties);
+    
+    return result;
+}
