86534a5d248598831bc0942e65888499793d2a58
NikitaPirat <nikita_fed_92-92@mail.ru>
2016-05-18 00:09:11 +0400

    properties names and texts added

--- Constructor/js/properties.js
+++ Constructor/js/properties.js
@@ -1,14 +1,30 @@
-var sharedProperties = [ 'name', 'obj_class', 'obj_type']
-var roadProperties = [ 'lines', 'marking'];
-var lineProperties = ['left', 'right'];
-var carProperties = ['number', 'right direction indicator', 'left direction indicator'];
+var sharedProperties = [ 
+    { name: 'name', text: 'name' }, 
+    { name: 'obj_class', text: 'class'}, 
+    { name: 'obj_type', text: 'type'}
+];
+var roadProperties = [ { name: 'lines', text: 'lines' }, { name: 'markup', text: 'markup'} ];
+var lineProperties = [ { name: 'uleft', text: 'left' }, { name: 'uright', text: 'right' }];
+var carProperties = [ 
+    { name: 'number', text: 'number' }, 
+    { name: 'rd_indicator', text: 'right direction indicator' }, 
+    { name: 'ld_indicator', text: 'left direction indicator' }
+];
 
 function createJsonProperties(){
     var result = [];
-    Array.prototype.push.apply(result, sharedProperties);
-    Array.prototype.push.apply(result, roadProperties);
-    Array.prototype.push.apply(result, lineProperties);
-    Array.prototype.push.apply(result, carProperties);
+    for (var i=0;i<sharedProperties.length;i++){
+        result[result.length] = sharedProperties[i].name;
+    }
+    for (var i=0;i<roadProperties.length;i++){
+        result[result.length] = roadProperties[i].name;
+    }
+    for (var i=0;i<lineProperties.length;i++){
+        result[result.length] = lineProperties[i].name;
+    }
+    for (var i=0;i<carProperties.length;i++){
+        result[result.length] = carProperties[i].name;
+    }
     
     return result;
 }
